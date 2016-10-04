5149406b5b71daab54faf8f0a2bd3097a96f7ccd
Diego Hernandes <diego@hernandev.com>
2016-10-04 04:19:48 -0300

    throttles logins

--- app/Units/Authentication/Http/Controllers/LoginController.php
+++ app/Units/Authentication/Http/Controllers/LoginController.php
@@ -2,7 +2,10 @@
 namespace Confee\Units\Authentication\Http\Controllers;
 
 use Confee\Support\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Lang;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 /**
@@ -10,17 +13,39 @@ use Tymon\JWTAuth\Exceptions\JWTException;
  */
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
+    /**
+     * Issue a JWT token when valid login credentials are
+     * presented.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = app('tymon.jwt.auth')->attempt($credentials)) {
+                // Increments login attempts
+                $this->incrementLoginAttempts($request);
+
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
         } catch (JWTException $e) {
+            // Increments login attempts
+            $this->incrementLoginAttempts($request);
+
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
@@ -28,4 +53,28 @@ class LoginController extends Controller
         // all good so return the token
         return response()->json(compact('token'));
     }
+
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Redirect the user after determining they are locked out.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds]);
+
+        return response()
+            ->json(['error' => $message], Response::HTTP_TOO_MANY_REQUESTS);
+    }
+
 }
